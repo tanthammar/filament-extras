664ea230a172155978ae879028ccade8d359e790
Tina Hammar <tinahammar@gmail.com>
2022-11-17 18:01:56 +0100

    TodoField/dummyField
    Make a note to self (TODO) to create a custom field, later...
    Shows the TODO/task as a notification in your forms.
    Throws an error if you try to set your project live without completing the task.
    TodoField::make('Create a custom positioning field')

---
+++ resources/views/components/todo-field.blade.php
@@ -0,0 +1,16 @@
+<div class="rounded-md bg-orange-100 p-4">
+    <div class="flex">
+        <div class="flex-shrink-0">
+            <!-- Heroicon name: mini/exclamation-triangle -->
+            <svg class="h-5 w-5 text-orange-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M8.485 3.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 3.495zM10 6a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 6zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="ml-3">
+            <h3 class="text-sm font-bold text-orange-800">TODO field …</h3>
+            <div class="mt-2 text-sm text-orange-700">
+                <p>{{ $task }}</p>
+            </div>
+        </div>
+    </div>
+</div>
