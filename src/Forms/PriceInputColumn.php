92f73adc958672219fe29bbd7070a6985e12181f
Tina Hammar <tinahammar@gmail.com>
2023-03-15 14:37:40 +0100

    fix PriceInput

--- src/Forms/PriceInputColumn.php
+++ src/Forms/PriceInputColumn.php
@@ -6,10 +6,10 @@ use Filament\Tables\Columns\TextInputColumn;
 
 class PriceInputColumn
 {
-    public static function make(): void
+    public static function make(): TextInputColumn
     {
-        TextInputColumn::make('price')
-            ->updateStateUsing(fn ($state) => $state === '' ? null : $state)
+        return TextInputColumn::make('price')
+            ->updateStateUsing(fn ($state, $record) => $record->update(['price' => $state === '' ? null : $state]))
             ->inputMode('decimal')
             ->step('0.50')
             ->type('number')
