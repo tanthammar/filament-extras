25dd94b27ca5ee7d03abce003393059130b65ff7
Tina Hammar <tinahammar@gmail.com>
2023-04-12 11:08:46 +0200

    Update AddressSearch.php

--- src/Forms/AddressSearch.php
+++ src/Forms/AddressSearch.php
@@ -15,17 +15,21 @@ class AddressSearch
             ->searchable()
             ->ignored()
             ->reactive()
-            ->getSearchResultsUsing(fn (string $search) => Nominatim::search($search));
+            ->getSearchResultsUsing(fn(string $search) => Nominatim::search($search));
 
         return $replaceFormData
             ? $field->afterStateUpdated(function ($livewire, $state) {
                 if ($state) {
+                    //DAN HARRIN 'data' does not exist in a relation manager
                     $livewire->data = Nominatim::lookup(osm_id: $state, existingFieldValue: $livewire->data);
                 }
             })
-            : $field->afterStateUpdated(function ($set, $get, $state) use ($jsonColumnName) {
+            : $field->afterStateUpdated(function ($get, $set, $state, $livewire, $component) use ($jsonColumnName) {
                 if ($state) {
-                    $set($jsonColumnName, Nominatim::lookup(osm_id: $state, existingFieldValue: $get($jsonColumnName)));
+                    //this worked when 'address' was a json column and not a relation
+                    //$set($jsonColumnName, Nominatim::lookup(osm_id: $state, existingFieldValue: $get($jsonColumnName)));
+                    //this works when 'address' is a relation or is a json column
+                    data_set($livewire->data, $jsonColumnName, Nominatim::lookup(osm_id: $state, existingFieldValue: data_get($livewire->data, $jsonColumnName, [])));
                 }
             });
 
