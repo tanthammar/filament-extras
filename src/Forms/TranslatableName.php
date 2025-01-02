76e25516f27feccc8759ab7075940a6042b48aa5
Tina Hammar <tinahammar@gmail.com>
2025-01-02 13:33:06 +0100

    optionally unique translatable name

--- src/Forms/TranslatableName.php
+++ src/Forms/TranslatableName.php
@@ -9,27 +9,31 @@ use Filament\Forms\Components\TextInput;
  */
 class TranslatableName
 {
-    public static function make(string $column = 'name', string $label = 'fields.name'): array
+    public static function make(?string $column = 'name', ?string $label = 'fields.name', ?bool $unique = false): array
     {
-        return [
-            TextInput::make("$column.sv")
-                ->label(trans($label) . ' Svenska')
-                ->ucfirst()
-                ->minLength(2)
-                ->maxLength(125)
-                ->requiredWithout("$column.en")
-                ->saveAs(fn ($get, $state) => $state ?: $get("$column.en"))
-                ->rules(['bail', 'alpha_dash_space_and'])
-                ->unique(column: $column . '->sv', ignoreRecord: true),
-            TextInput::make("$column.en")
-                ->label(trans($label) . ' English')
-                ->ucfirst()
-                ->minLength(2)
-                ->maxLength(125)
-                ->requiredWithout("$column.sv")
-                ->saveAs(fn ($get, $state) => $state ?: $get("$column.sv"))
-                ->rules(['bail', 'alpha_dash_space_and'])
-                ->unique(column: $column . '->en', ignoreRecord: true),
-        ];
+        $svField = TextInput::make("$column.sv")
+            ->label(trans($label) . ' Svenska')
+            ->ucfirst()
+            ->minLength(2)
+            ->maxLength(125)
+            ->requiredWithout("$column.en")
+            ->saveAs(fn ($get, $state) => $state ?: $get("$column.en"))
+            ->rules(['alpha_dash_space_and']);
+
+        $enField = TextInput::make("$column.en")
+            ->label(trans($label) . ' English')
+            ->ucfirst()
+            ->minLength(2)
+            ->maxLength(125)
+            ->requiredWithout("$column.sv")
+            ->saveAs(fn ($get, $state) => $state ?: $get("$column.sv"))
+            ->rules(['alpha_dash_space_and']);
+
+        if ($unique) {
+            $svField->unique(column: $column . '->sv', ignoreRecord: true);
+            $enField->unique(column: $column . '->en', ignoreRecord: true);
+        }
+
+        return [$svField, $enField];
     }
 }
